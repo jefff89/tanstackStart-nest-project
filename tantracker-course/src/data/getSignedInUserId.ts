a1898d6720e811ac998edbe9e43d50d4b95b2760
unknown <jefff89>
2025-11-22 20:43:02 +0330

    api connected to tanstackStart

--- tantracker-course/src/data/getSignedInUserId.ts
+++ tantracker-course/src/data/getSignedInUserId.ts
@@ -3,7 +3,8 @@ import { createServerFn } from '@tanstack/react-start'
 export const getSingedInUserId = createServerFn({
   method: 'GET',
 }).handler(async () => {
-  const user = await fetch('https://api.github.com/users/marcos-costa') // Fetch the signed in GitHub User ID from Github API
-    .then((res) => res.json())
-  return user?.userId
+  const user = await fetch('http://localhost:3001/auth/2').then((res) =>
+    res.json(),
+  )
+  return user?.id
 })
--- tantracker-course/src/routes/_authed.tsx
+++ tantracker-course/src/routes/_authed.tsx
@@ -1,14 +1,14 @@
 import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_authed')({
-  // beforeLoad({ context }) {
-  //   if (!context.userId) {
-  //     redirect({
-  //       to: '/',
-  //       throw: true,
-  //     })
-  //   }
-  // },
+  beforeLoad({ context }) {
+    if (!context.userId) {
+      redirect({
+        to: '/',
+        throw: true,
+      })
+    }
+  },
   component: RouteComponent,
 })
 
